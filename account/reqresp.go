65b344dc2da97a81f50b8e9d7348e96fdeba9630
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-11 09:08:34 +0100

    create server

--- account/reqresp.go
+++ account/reqresp.go
@@ -1,5 +1,13 @@
 package account
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type (
 	CreateUserRequest struct {
 		Email    string `json:"email"`
@@ -15,3 +23,27 @@ type (
 		Id string `json:"id"`
 	}
 )
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, respone any) error {
+	return json.NewEncoder(w).Encode(respone)
+}
+
+func decodeUserReq(ctx context.Context, r *http.Request) (any, error) {
+	var req CreateUserRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func decodeEmailReq(ctx context.Context, r *http.Request) (any, error) {
+	var req GetUserRequest
+	vars := mux.Vars(r)
+
+	req = GetUserRequest{
+		Id: vars["id"],
+	}
+
+	return req, nil
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.19
 
 require (
 	github.com/go-kit/kit v0.12.0
+	github.com/go-kit/log v0.2.0
 	github.com/gofrs/uuid v4.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
 )
 
-require (
-	github.com/go-kit/log v0.2.0 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
-)
+require github.com/go-logfmt/logfmt v0.5.1 // indirect
